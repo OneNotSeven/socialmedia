171cd614ace09645830f5fc3e3a6e8b6701946e5
Aman <160247525+OneNotSeven@users.noreply.github.com>
2025-03-22 14:28:00 +0530

    Update page.tsx

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -41,17 +41,20 @@ const Page = () => {
         <Uploadcontent user={ userId}/>
         <Tweets/>
       </div>
-      <div className='sm:flex hidden flex-col h-fit  sticky top-0 mr-5 ml-[44px] gap-4'>
-
-        {userId && <CommunitySearch userId={userId} />}
-        <WhatsHappening />
-            <FollowSuggestion userid={ userId} />
-        <div className='text-[12px] mb-4 w-[366px]' >
-          <span>Terms & conditon</span><span className='pl-2'>policy</span><span className='pl-2'>services</span><span className="pl-2">Accessibility</span><span className="pl-2">About</span>
-
-          <p className="">@2025 Twins created by Aman Jha</p>
-        </div>
-      </div>
+     <div className="sm:flex hidden flex-col sticky top-0 mr-5 ml-[44px] gap-4 
+                max-h-screen overflow-y-auto scrollbar-hide">
+  {userId && <CommunitySearch userId={userId} />}
+  <WhatsHappening />
+  <FollowSuggestion userid={userId} />
+  <div className="text-[12px] mb-4 w-[366px]">
+    <span>Terms & Condition</span>
+    <span className="pl-2">Policy</span>
+    <span className="pl-2">Services</span>
+    <span className="pl-2">Accessibility</span>
+    <span className="pl-2">About</span>
+    <p>@2025 Twins created by Aman Jha</p>
+  </div>
+</div>
           </>
      }
       
